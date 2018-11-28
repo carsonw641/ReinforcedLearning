16374759228ae08824b0414fd26480d2422dfb17
carsonw641 <carson.w641@gmail.com>
2018-11-28 00:52:20 -0700

    Set up state and actions objects along with initializing states

--- sarsa&qLearning.cpp
+++ sarsa&qLearning.cpp
@@ -1,21 +1,82 @@
 #include <iostream>
+#include <vector>
+#include <memory>
+
+const int rows = 4, columns = 12;
+
+enum Direction{
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST,
+    NONE
+};
+
+
+class Action{
+public:
+    double value = 0;
+    Direction direction = NONE;
+
+    Action(Direction d): direction{d}{}
+    Action(Action&& a): value{a.value}, direction{a.direction} {}
+
+};
+
+class State{
+public:
+    std::vector<Action> availableActions;
+    Direction policy = NONE;
+
+    State(){}
+};
+
+int detReward(int row, int column){
+    if (row == rows-1 && column > 0 && column < columns-1){
+        return -100;
+    }else if (row == rows-1 && column == columns-1){
+        return 1;
+    }
+
+    return -1;
+};
 
 int main(){
-    int rows = 4, columns = 12;
+    
+    int row = rows-1;
+    int column = 0;
 
-    int map[rows][columns];
+    for (int iterations = 0; iterations < 10000; iterations++){
+
+    }
+
+    State states[rows][columns];
 
 
-    double alpha = .9, eGreedy = .1, discount = -.5;
+    double alpha = .1, eGreedy = .1; 
 
-
-    // Generate Cliff map
-    for (int i = rows-1; i < rows; i++){
+    // initialize states w/ available actions
+    for (int i = 0; i < rows; i++){
         for (int j = 0; j < columns; j++){
-            if (j != 0 && j != columns -1){
-                map[i][j] = -100;
-            }else if (j == columns-1){
-                map[i][j] = 1;
+            //up
+            if (i > 0){
+                Action action(NORTH);
+                states[i][j].availableActions.push_back(std::move(action));
+            }
+            //down
+            if (i < rows-1){
+                Action action(SOUTH);
+                states[i][j].availableActions.push_back(std::move(action));
+            }
+            //left
+            if (j > 0){
+                Action action(WEST);
+                states[i][j].availableActions.push_back(std::move(action));
+            }
+            //right
+            if (j < columns-1){
+                Action action(EAST);
+                states[i][j].availableActions.push_back(std::move(action));
             }
         }
     }
